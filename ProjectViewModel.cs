3821d18db474339f077c8f8811e79e85afc9f649
Setareh pishyar <140304083+Setarehpishyar@users.noreply.github.com>
2025-03-17 11:48:00 +0000

    ProjectViewModel.cs

--- ProjectViewModel.cs
+++ ProjectViewModel.cs
@@ -1,19 +1,14 @@
-﻿using System.ComponentModel;
-using System.Collections.ObjectModel;
 using ProjectManagementApp.Models;
 using ProjectManagementApp.Data;
+using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace ProjectManagementApp.ViewModels
 {
-    public class ProjectViewModel : INotifyPropertyChanged
+    public class ProjectViewModel
     {
-        
-        public event PropertyChangedEventHandler? PropertyChanged = delegate { };
+        private readonly AppDbContext _context;
 
-        private readonly AppDbContext _context; 
-
-        
         public ObservableCollection<Project> Projects { get; set; } = new ObservableCollection<Project>();
 
         private Project _selectedProject;
@@ -22,25 +17,14 @@ namespace ProjectManagementApp.ViewModels
             get { return _selectedProject; }
             set
             {
-                if (_selectedProject != value)
-                {
-                    _selectedProject = value;
-                    OnPropertyChanged(nameof(SelectedProject));  
-                }
+                _selectedProject = value;
             }
         }
 
-      
         public ProjectViewModel(AppDbContext context)
         {
             _context = context;
-
-            
             Projects = new ObservableCollection<Project>(_context.Projects.ToList());
-        }
-
-        public ProjectViewModel()
-        {
         }
 
         public void AddProject(Project project)
@@ -48,14 +32,12 @@ namespace ProjectManagementApp.ViewModels
             _context.Projects.Add(project);
             _context.SaveChanges();
             Projects.Add(project);
-            OnPropertyChanged(nameof(Projects));
         }
 
         public void UpdateProject(Project project)
         {
             _context.Projects.Update(project);
             _context.SaveChanges();
-            OnPropertyChanged(nameof(Projects));
         }
 
         public void DeleteProject(Project project)
@@ -63,12 +45,7 @@ namespace ProjectManagementApp.ViewModels
             _context.Projects.Remove(project);
             _context.SaveChanges();
             Projects.Remove(project);
-            OnPropertyChanged(nameof(Projects));
-        }
-
-        private void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
+
